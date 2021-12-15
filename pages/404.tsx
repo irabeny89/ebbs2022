a0ca561cb5bb40259588e2943eb5213d37e57f5d
Ernest Irabor <irabeny89@gmail.com>
2021-12-15 16:49:03 +0100

    error pages

---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+import ErrorPage from "@/components/ErrorPage"
+
+const Custom404 = () => <ErrorPage title="Page Not Found" message="404 - Page Not Found" />
+
+export default Custom404
---
+++ pages/500.tsx
@@ -0,0 +1,5 @@
+import ErrorPage from "@/components/ErrorPage"
+
+const Custom500 = () => <ErrorPage title="Server Error" message="500 - Server Error" />
+
+export default Custom500
