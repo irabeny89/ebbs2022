e690d0cd13f5f6d6becca89a2f96e3f0d1fcdd6e
Ernest Irabor <irabeny89@gmail.com>
2021-12-18 10:09:11 +0100

    refresh token refactored

--- utils/refreshToken.ts
+++ utils/refreshToken.ts
@@ -1,43 +1,56 @@
 import { verify, JwtPayload } from "jsonwebtoken";
 import { GraphContextType, UserPayloadType } from "types";
 import config from "config";
-import { AUTHORIZATION_ERROR_MESSAGE, authUser, handleError } from ".";
+import { authUser, handleError } from ".";
 import { AuthenticationError } from "apollo-server-micro";
+import mongoose from "mongoose";
 
-const refreshToken = async (
-  _: any,
-  __: any,
-  { req: { cookies }, res, RefreshTokenModel }: GraphContextType
-) => {
-  try {
-    // verify refresh token
-    const { aud, sub, username } = verify(
-        cookies?.token,
-        config.environmentVariable.jwtRefreshSecret
-      ) as JwtPayload & Pick<UserPayloadType, "username">,
-      // re-auth user
-      tokenPair = authUser(
-        {
-          id: sub!,
-          audience: aud as "ADMIN" | "USER",
-          username,
-        },
-        res
-      );
-    // update refresh token document
-    await RefreshTokenModel.findOneAndUpdate(
-      {
-        token: cookies?.token,
+const {
+    environmentVariable: { jwtRefreshSecret },
+    appData: { generalErrorMessage },
+  } = config,
+  refreshToken = async (
+    _: any,
+    __: any,
+    {
+      req: {
+        cookies: { token },
       },
-      {
-        token: tokenPair?.refreshToken,
-      }
-    ).exec();
+      res,
+      RefreshTokenModel,
+    }: GraphContextType
+  ) => {
+    try {
+      // verify refresh token
+      const { aud, sub, username } = verify(
+          token,
+          jwtRefreshSecret
+        ) as JwtPayload & Pick<UserPayloadType, "username">,
+        // re-auth user
+        tokenPair = authUser(
+          {
+            id: sub!,
+            audience: aud as "ADMIN" | "USER",
+            username,
+          },
+          res
+        );
+      // update refresh token document using token
+      await RefreshTokenModel.findOneAndUpdate(
+        {
+          token,
+        },
+        {
+          token: tokenPair?.refreshToken,
+        }
+      ).exec();
+      // disconnect db
+      mongoose.disconnect();
 
-    return tokenPair;
-  } catch (error) {
-    handleError(error, AuthenticationError, AUTHORIZATION_ERROR_MESSAGE);
-  }
-};
+      return tokenPair;
+    } catch (error) {
+      handleError(error, AuthenticationError, generalErrorMessage);
+    }
+  };
 
 export default refreshToken;
