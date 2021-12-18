7bf4824080fa215b4c58761513d5098e17f92e61
Ernest Irabor <irabeny89@gmail.com>
2021-12-18 09:53:45 +0100

    get profile refactored

--- utils/getMyProfile.ts
+++ utils/getMyProfile.ts
@@ -1,5 +1,9 @@
 import { GraphContextType } from "types";
-import { handleAuth } from ".";
+import { getAuthPayload, handleError } from ".";
+import config from "config";
+import mongoose from "mongoose"
+
+const { generalErrorMessage } = config.appData;
 
 const getMyProfile = async (
   _: any,
@@ -10,9 +14,21 @@ const getMyProfile = async (
       headers: { authorization },
     },
   }: GraphContextType
-) =>
-  await UserModel.findById(
-    handleAuth(authorization!.replace("Bearer ", ""))?.sub
-  ).exec();
+) => {
+  try {
+    // auth users only, returns user profile; getAuthPayload throws error if user has no auth
+    const user = await UserModel.findById(
+      getAuthPayload(authorization!.replace("Bearer ", ""))?.sub
+    )
+      .populate("ratedBusinesses requests business wallet withdraws")
+      .exec();
+    // disconnect db
+    mongoose.disconnect()
+      
+    return user
+  } catch (error: any) {
+    handleError(error.message, Error, generalErrorMessage);
+  }
+};
 
 export default getMyProfile;
