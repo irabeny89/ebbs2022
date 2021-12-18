c67316a63c54a744965101d9af7069e1aece71cb
Ernest Irabor <irabeny89@gmail.com>
2021-12-18 10:26:49 +0100

    refactor get users for admin user

--- utils/getUsers.ts
+++ utils/getUsers.ts
@@ -1,5 +1,8 @@
+import { AuthenticationError } from "apollo-server-micro";
 import { GraphContextType } from "types";
-import { isAdminUser } from ".";
+import { getAuthPayload, handleError } from ".";
+import config from "config";
+import mongoose from "mongoose";
 
 // admin users only query
 const getUsers = async (
@@ -12,17 +15,28 @@ const getUsers = async (
     },
   }: GraphContextType
 ) => {
-  // only admin allowed
-  if (isAdminUser(authorization!.replace("Bearer ", "")))
-    return await UserModel.find()
-      .populate("business")
-      .populate("wallet")
-      .populate("withdraws")
-      .populate("requests")
-      .populate("ratedBusinesses")
-      .exec();
+  try {
+    // only admin allowed
+    if (getAuthPayload(authorization!).aud === "ADMIN")
+      return await UserModel.find()
+        .populate("business")
+        .populate("wallet")
+        .populate("withdraws")
+        .populate("requests")
+        .populate("ratedBusinesses")
+        .exec();
+    // disconnect db
+    mongoose.disconnect();
 
-  // do nothing(not even error) for unauthorized/non-admin user
+    // do nothing(not even error) for unauthorized/non-admin user
+  } catch (error: any) {
+    // log error for more
+    handleError(
+      error.message,
+      AuthenticationError,
+      config.appData.generalErrorMessage
+    );
+  }
 };
 
 export default getUsers;
