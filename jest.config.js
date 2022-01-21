54a6b5e3162271e9bd21f04e12778e07725265a1
Ernest Irabor <irabeny89@gmail.com>
2022-01-21 20:16:07 +0100

    ⚡️ 🎨  update update to use the nextjs swc

--- jest.config.js
+++ jest.config.js
@@ -5,40 +5,7 @@ createJestConfig = nextJest({
 })
 
 module.exports = createJestConfig({
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
-    // https://jestjs.io/docs/webpack#mocking-css-modules
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
-    // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-
-    // Handle image imports
-    // https://jestjs.io/docs/webpack#handling-static-assets
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/__mocks__/fileMock.js`,
-
-    // Path alias
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/models/(.*)$': '<rootDir>/models/$1',
-    '^@/graphql/(.*)$': '<rootDir>/graphql/$1',
-    '^@/styles/(.*)$': '<rootDir>/styles/$1',
-    '^@/utils/(.*)$': '<rootDir>/utils/$1'
-  },
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: "jest-environment-jsdom"
 })
