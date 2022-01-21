b139ef98c7c68908ed1cbd4af14209ff338a7324
Ernest Irabor <irabeny89@gmail.com>
2022-01-21 23:22:11 +0100

    ✅ pass test

---
+++ __tests__/components/ProductList.test.tsx
@@ -0,0 +1,19 @@
+import { MockedProvider } from "@apollo/client/testing";
+import { render, cleanup } from "@testing-library/react";
+import ProductList from "@/components/ProductList";
+import { productProps } from "@/models/mockData";
+
+const items = [productProps, { ...productProps, _id: "2" }];
+
+describe("Product List Component", () => {
+  afterEach(cleanup);
+  it("renders product list", () => {
+    expect(
+      render(
+        <MockedProvider mocks={[]}>
+          <ProductList items={items} />
+        </MockedProvider>
+      ).getAllByText(productProps.name)
+    ).toHaveLength(2);
+  });
+});
