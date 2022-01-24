fa81af6b130f83cc76f62eefaeb0640412c08403
Ernest Irabor <irabeny89@gmail.com>
2022-01-24 11:28:07 +0100

    🔧 add path alias

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
 const nextJest = require("next/jest"),
 
-createJestConfig = nextJest({
-  dir: "./"
-})
+  createJestConfig = nextJest({
+    dir: "./"
+  })
 
 module.exports = createJestConfig({
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: "jest-environment-jsdom"
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    // Path alias
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/models/(.*)$': '<rootDir>/models/$1',
+    '^@/graphql/(.*)$': '<rootDir>/graphql/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1'
+  }
 })
