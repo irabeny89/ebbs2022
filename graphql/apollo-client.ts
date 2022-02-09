7d1b962ed90d2de57861d3ac70e6ab0fa2f256b9
Ernest Irabor <irabeny89@gmail.com>
2022-02-09 02:08:10 +0100

    🎨 ⚡️ 🐛 🚑 add auto-auth

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { RetryLink } from "@apollo/client/link/retry";
-import { accessTokenVar } from "@/graphql/reactiveVariables";
 import config from "../config";
+import { accessTokenVar } from "@/graphql/reactiveVariables";
 import axios from "axios";
 
 const {
@@ -10,69 +10,58 @@ const {
   appData: { abbr },
 } = config;
 
-const httpLink = new HttpLink({
-    uri: host + graphqlUri,
-  }),
-
-  errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
-    if (graphQLErrors) {
-      graphQLErrors.forEach(
-        async ({ message, locations, path, extensions: { code } }) => {
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${JSON.stringify(path)}`
-          );
-  console.log('===apollo-client:error link========================');
-  console.log(code);
-  console.log('====================================');
-          switch (code) {
-            case "UNAUTHENTICATED":
-              try {
-                const {
-                  data: { refreshToken },
-                } = await axios.post(host + graphqlUri, {
-                  query: "query{refreshToken}",
-                });
-                console.log('=================apollo-client.ts==============');
-                console.log(refreshToken);
-                console.log('====================================');
-                refreshToken && accessTokenVar(refreshToken);
-
-                operation.setContext({
-                  headers: {
-                    ...operation.getContext().headers,
-                    authorization: `Bearer ${refreshToken}`,
-                  },
-                });
-
-                return forward(operation);
-              } catch (err) {
-                console.error(err, "axios refreshToken error");
-              } finally {
-                return forward(operation);
+const client = new ApolloClient({
+  name: abbr,
+  version: "1.0.0",
+  cache: new InMemoryCache(),
+  link: from(
+    [
+      onError(({ graphQLErrors, networkError, operation, forward }) => {
+        if (graphQLErrors) {
+          graphQLErrors.forEach(
+            async ({ message, locations, path, extensions: { code } }) => {
+              console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
+                  locations
+                )}, Path: ${JSON.stringify(path)}, Code: ${code}`
+              );
+              if (code === "UNAUTHENTICATED") {
+                try {
+                  const {
+                    data: {
+                      data: { refreshToken },
+                    },
+                  } = await axios.post<
+                    Record<"data", { refreshToken: string }>
+                  >(host + graphqlUri, {
+                    query: "query{refreshToken}",
+                  });
+                  accessTokenVar(refreshToken);
+                  operation.setContext({
+                    headers: {
+                      authorization: `Bearer ${accessTokenVar()}`,
+                    },
+                  });
+                  return forward(operation);
+                } catch (error) {
+                  console.error(error);
+                }
               }
-          }
+            }
+          );
         }
-      );
-    }
-
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  }),
-  retryLink = new RetryLink({
-    delay: {
-      initial: 300,
-      max: Infinity,
-      jitter: true,
-    },
-    attempts: {
-      max: 5,
-      retryIf: (error, _operation) => !!error,
-    },
-  }),
-  client = new ApolloClient({
-    name: abbr,
-    version: "1.0.0",
-    cache: new InMemoryCache(),
-    link: from([errorLink, retryLink, httpLink]),
-  });
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+        return forward(operation);
+      }),
+      new RetryLink(),
+      new HttpLink({
+        uri: host + graphqlUri,
+      }),
+      // log error in dev; i.e remove error link in production
+    ].filter((_, i) =>
+      process.env.NODE_ENV === "development" ? true : i !== 1
+    )
+  ),
+});
 
 export default client;
