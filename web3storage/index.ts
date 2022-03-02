666e2d2c25b5729a9d334830b0aabd93d7551672
Ernest Irabor <irabeny89@gmail.com>
2022-03-02 03:51:00 +0100

    fix bug

--- web3storage/index.ts
+++ web3storage/index.ts
@@ -2,6 +2,6 @@ import config from "../config";
 import { Web3Storage } from "web3.storage";
 
 const web3storage = new Web3Storage({
-  token: config.environmentVariable.web3storageKey,
+  token: config.environmentVariable.web3storageKey!,
 });
 export default web3storage;
