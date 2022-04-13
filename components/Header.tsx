090ed5ec60958136e32650749530a2dc0d38f1e9
Ernest Irabor <irabeny89@gmail.com>
2022-04-13 02:22:47 +0100

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "./NavigationBar";
+import SubHeader from "./SubHeader";
+
+export default function Header() {
+  return (
+    <header>
+      <NavigationBar />
+      <SubHeader />
+    </header>
+  );
+}
