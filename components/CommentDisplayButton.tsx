b1c053071a73c975fdf6a7d862f331368506d0ff
Ernest Irabor <irabeny89@gmail.com>
2022-04-18 10:23:12 +0100

    add comment display button

---
+++ components/CommentDisplayButton.tsx
@@ -0,0 +1,48 @@
+import Button from "react-bootstrap/Button";
+import { COMMENT_COUNT } from "@/graphql/documentNodes";
+import { useQuery } from "@apollo/client";
+import { CommentDisplayButtonPropsType, ServiceVertexType } from "types";
+import { BiMessageAltDots } from "react-icons/bi";
+import getCompactNumberFormat from "@/utils/getCompactNumberFormat";
+import { useState } from "react";
+import AjaxFeedback from "./AjaxFeedback";
+import ServiceCommentModal from "./ServiceCommentModal";
+
+export default function CommentDisplayButton({
+  serviceId,
+}: CommentDisplayButtonPropsType) {
+  const [showComment, setShowComment] = useState(false),
+    { data, loading, error } = useQuery<
+      Record<"service", ServiceVertexType>,
+      Record<"serviceId", string>
+    >(COMMENT_COUNT, {
+      variables: {
+        serviceId,
+      },
+    });
+
+  return loading ? (
+    <AjaxFeedback loading />
+  ) : error ? (
+    <AjaxFeedback error={error} />
+  ) : (
+    <>
+      <ServiceCommentModal
+        {...{
+          serviceId,
+          setShow: setShowComment,
+          show: showComment,
+        }}
+      />
+      <Button
+        size="sm"
+        className="py-0 w-100"
+        variant="outline-secondary"
+        onClick={() => setShowComment(true)}
+      >
+        <BiMessageAltDots size={18} />{" "}
+        {getCompactNumberFormat(data?.service?.commentCount! ?? 0)}
+      </Button>
+    </>
+  );
+}
