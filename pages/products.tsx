1cc597512eb3766014fcf182bf0a8a27cb1e8e9e
Ernest Irabor <irabeny89@gmail.com>
2022-04-21 21:31:27 +0100

    ♻️ 🐛 refactor & bug fix

--- pages/products.tsx
+++ pages/products.tsx
@@ -27,87 +27,87 @@ const { webPages, abbr } = config.appData,
   );
 // fetch data at build time
 export const getStaticProps: GetStaticProps = async () => {
-    const { data, error } = await client.query<
-      Record<"products", CursorConnectionType<ProductCardPropsType>>,
-      Record<"args", PagingInputType>
-    >({
-      query: FEW_PRODUCTS,
-      variables: { args: { last: 20 } },
-      fetchPolicy: "no-cache",
-    });
+  const { data, error } = await client.query<
+    Record<"products", CursorConnectionType<ProductCardPropsType>>,
+    Record<"args", PagingInputType>
+  >({
+    query: FEW_PRODUCTS,
+    variables: { args: { last: 25 } },
+    fetchPolicy: "no-cache",
+  });
 
-    return error
-      ? { notFound: true }
-      : { props: data.products, revalidate: 20 };
-  },
-  // products page component
-  ProductsPage = ({
-    edges,
-    pageInfo: { endCursor, hasNextPage },
-  }: CursorConnectionType<ProductCardPropsType>) => {
-    // ref for lazy fetch flag
-    const hasLazyFetched = useRef(false);
-    // omit cursor property from edge node
-    const products = edges.map((item) => item.node);
+  return error ? { notFound: true } : { props: data.products, revalidate: 15 };
+};
+// products page component
+export default function ProductsPage({
+  edges,
+  pageInfo: { startCursor, hasPreviousPage },
+}: CursorConnectionType<ProductCardPropsType>) {
+  // ref for lazy fetch flag
+  const hasLazyFetched = useRef(false),
     // lazy fetch more products
-    const [fetchMoreProducts, { data, loading, fetchMore }] = useLazyQuery<
+    [fetchProducts, { data, loading, fetchMore }] = useLazyQuery<
       Record<"products", CursorConnectionType<ProductCardPropsType>>,
       Record<"args", PagingInputType>
     >(FEW_PRODUCTS, {
       variables: {
-        args: { last: 20, before: endCursor },
+        args: { last: 25, before: startCursor },
       },
-    });
+      notifyOnNetworkStatusChange: true,
+    }),
+    handleFetchMore = () =>
+      fetchMore({
+        variables: {
+          args: {
+            last: 25,
+            before: data?.products?.pageInfo?.startCursor,
+          },
+        },
+      }),
+    products = edges
+      .map((item) => item.node)
+      .concat(data?.products?.edges?.map((edge) => edge.node) ?? []),
+    showMoreButton = data
+      ? data?.products?.pageInfo?.hasPreviousPage
+      : hasPreviousPage;
 
-    return (
-      <Layout>
-        {/* head title for tabs */}
-        <Head>
-          <title>
-            {abbr} | {productsPage?.pageTitle}
-          </title>
-        </Head>
-        <PageIntro
-          pageTitle={
-            <>
-              <FaBoxes size="40" className="mb-2" /> {productsPage?.pageTitle}
-            </>
-          }
-          paragraphs={productsPage?.parargraphs}
-        />
-        <SortedListWithTabs
-          ListRenderer={ProductSection}
-          field="category"
-          list={products
-            .concat(data?.products.edges.map((edge) => edge.node) ?? [])
-            .reverse()}
-          rendererProps={{ className: "pt-4 rounded" }}
-        />
-        <Row>
-          <Col>
-            {hasNextPage ? (
-              <MoreButton
-                {...{
-                  customFetch: fetchMoreProducts,
-                  fetchMore: () =>
-                    fetchMore({
-                      variables: {
-                        args: {
-                          last: 20,
-                          before: data?.products?.pageInfo?.endCursor,
-                        },
-                      },
-                    }),
-                  hasLazyFetched,
-                  label: "More products",
-                  loading,
-                }}
-              />
-            ) : null}
-          </Col>
-        </Row>
-      </Layout>
-    );
-  };
-
-export default ProductsPage;
+  return (
+    <Layout>
+      {/* head title for tabs */}
+      <Head>
+        <title>
+          {abbr} | {productsPage?.pageTitle}
+        </title>
+      </Head>
+      <PageIntro
+        pageTitle={
+          <>
+            <FaBoxes size="40" className="mb-2" /> {productsPage?.pageTitle}
+          </>
+        }
+        paragraphs={productsPage?.parargraphs}
+      />
+      <SortedListWithTabs
+        ListRenderer={ProductSection}
+        field="category"
+        list={products}
+        rendererProps={{ className: "pt-4" }}
+      />
+      <Row>
+        <Col>
+          {showMoreButton && (
+            <MoreButton
+              {...{
+                initialFetch: fetchProducts,
+                fetchMore: handleFetchMore,
+                hasLazyFetched: (hasLazyFetched.current = !!data),
+                label: "More products",
+                loading,
+              }}
+            />
+          )}
+        </Col>
+      </Row>
+    </Layout>
+  );
+}
