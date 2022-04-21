4a9a2ae7fb034b7093b74d4011f154e19e3d2a7c
Ernest Irabor <irabeny89@gmail.com>
2022-04-21 21:07:57 +0100

    add graphql client cache

---
+++ graphql/cache.ts
@@ -0,0 +1,43 @@
+import { InMemoryCache } from "@apollo/client";
+import { CursorConnectionType, ProductVertexType, ServiceVertexType } from "types";
+
+const initialData = {
+  edges: [],
+  pageInfo: {
+    startCursor: "",
+    endCursor: "",
+    hasNextPage: false,
+    hasPreviousPage: false,
+  },
+};
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        products: {
+          keyArgs: false,
+          merge: (
+            existing: CursorConnectionType<ProductVertexType> = initialData,
+            incoming: CursorConnectionType<ProductVertexType>
+          ): CursorConnectionType<ProductVertexType> => ({
+            edges: [...existing.edges, ...incoming.edges],
+            pageInfo: incoming.pageInfo,
+          }),
+        },
+        services: {
+          keyArgs: false,
+          merge: (
+            existing: CursorConnectionType<ServiceVertexType> = initialData,
+            incoming: CursorConnectionType<ServiceVertexType>
+          ): CursorConnectionType<ServiceVertexType> => ({
+            edges: [...existing.edges, ...incoming.edges],
+            pageInfo: incoming.pageInfo,
+          }),
+        },
+      },
+    },
+  },
+});
+
+export default cache;
