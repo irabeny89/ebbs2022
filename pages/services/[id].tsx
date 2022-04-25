c84a2875fbeadfd0cd17804190983aa4015a9602
Ernest Irabor <irabeny89@gmail.com>
2022-04-25 03:54:59 +0100

    🐛 refactor code & bug fix

--- pages/services/[id].tsx
+++ pages/services/[id].tsx
@@ -6,7 +6,6 @@ import client from "@/graphql/apollo-client";
 import { GetStaticProps, GetStaticPaths } from "next";
 import {
   PagingInputType,
-  ProductCardPropType,
   ServiceReturnType,
   ServiceVariableType,
   ServiceVertexType,
@@ -34,11 +33,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const { data } = await client.query<ServiceReturnType, ServiceVariableType>({
     query: FEW_SERVICES,
     variables: {
-      commentArgs: {
-        last: 20,
-      },
       productArgs: {
-        last: 20,
+        last: 25,
       },
       serviceArgs: {
         first: 50,
@@ -55,114 +51,113 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    if (!params?.id) return { notFound: true };
-    try {
-      const { data, error } = await client.query<
-        {
-          service: ServiceVertexType;
-        },
-        { serviceId: string } & Record<
-          "productArgs" | "commentArgs",
-          PagingInputType
-        >
-      >({
-        query: SERVICE,
-        variables: {
-          serviceId: params.id as string,
-          productArgs: { last: 20 },
-          commentArgs: { last: 20 },
-        },
-      });
+  if (!params?.id) return { notFound: true };
+  try {
+    const { data, error } = await client.query<
+      {
+        service: ServiceVertexType;
+      },
+      { serviceId: string } & Record<"productArgs", PagingInputType>
+    >({
+      query: SERVICE,
+      variables: {
+        serviceId: params.id as string,
+        productArgs: { last: 25 },
+      },
+    });
 
-      return error || !data?.service
-        ? { notFound: true }
-        : { props: data.service, revalidate: 20 };
-    } catch (error) {
-      return { notFound: true };
-    }
-  },
-  // service page component
-  ServicePage = ({
-    products: productConnection,
-    ...rest
-  }: ServiceVertexType) => {
-    // ref for lazy fetch flag
-    const hasLazyFetched = useRef(false),
-      // query mutation
-      [fetchMoreProducts, { data, loading, fetchMore }] = useLazyQuery<
-        {
-          service: ServiceVertexType;
+    return error || !data?.service
+      ? { notFound: true }
+      : { props: data.service, revalidate: 15 };
+  } catch (error) {
+    return { notFound: true };
+  }
+};
+
+export default function ServicePage({
+  products: productConnection,
+  ...rest
+}: Required<ServiceVertexType>) {
+  // ref for lazy fetch flag
+  const hasLazyFetched = useRef(false),
+    // new data reference
+    productsRef = useRef(productConnection.edges),
+    // query mutation
+    [fetchProducts, { data, loading }] = useLazyQuery<
+      {
+        service: ServiceVertexType;
+      },
+      { serviceId: string; productArgs: PagingInputType }
+    >(SERVICE_PRODUCT, {
+      variables: {
+        productArgs: {
+          last: 25,
+          before: productConnection?.pageInfo.startCursor,
         },
-        { serviceId: string; productArgs: PagingInputType }
-      >(SERVICE_PRODUCT, {
+        serviceId: rest?._id?.toString()!,
+      },
+    }),
+    handleFetchMore = () =>
+      fetchProducts({
         variables: {
+          serviceId: rest._id.toString(),
           productArgs: {
-            last: 20,
-            before: productConnection?.pageInfo.endCursor,
+            last: 25,
+            before: data?.service.products?.pageInfo.startCursor,
           },
-          serviceId: rest?._id?.toString()!,
         },
-      });
-
-    return (
-      <Layout>
-        {/* head title for service page tab */}
-        <Head>
-          <title>{abbr} | Service</title>
-        </Head>
-        {/* when fetching data out of range of staticPaths */}
-        {useRouter().isFallback ? (
-          <AjaxFeedback loading />
-        ) : (
-          // service section
-          <Container>
-            <Row>
-              <ServiceLabel {...rest} className="my-5" />
-            </Row>
-            <Row>
-              {productConnection?.edges && (
-                <SortedListWithTabs
-                  ListRenderer={ProductSection}
-                  field="category"
-                  list={
-                    productConnection.edges
-                      .concat(data?.service?.products?.edges ?? [])
-                      .map((edge) => edge.node) as ProductCardPropType[]
-                  }
-                  rendererProps={{ className: "pt-4 rounded" }}
+      }),
+    showMoreButton = data
+      ? data.service.products?.pageInfo.hasPreviousPage
+      : productConnection.pageInfo.hasPreviousPage;
+  // deduplicate products data
+  productsRef.current = [
+    ...new Set(
+      productsRef.current.concat(data?.service?.products?.edges ?? [])
+    ),
+  ];
+  return (
+    <Layout>
+      {/* head title for service page tab */}
+      <Head>
+        <title>{abbr} | Service</title>
+      </Head>
+      {/* when fetching data out of range of staticPaths */}
+      {useRouter().isFallback ? (
+        <AjaxFeedback loading />
+      ) : (
+        // service section
+        <Container>
+          <Row>
+            <ServiceLabel {...rest} className="my-5" />
+          </Row>
+          <Row>
+            {productConnection?.edges && (
+              <SortedListWithTabs
+                ListRenderer={ProductSection}
+                field="category"
+                list={productsRef.current.map(({ node }) => node)}
+                rendererProps={{ className: "pt-4 rounded" }}
+              />
+            )}
+          </Row>
+          <Row className="my-4">
+            <Col>
+              {showMoreButton && (
+                <MoreButton
+                  {...{
+                    initialFetch: fetchProducts,
+                    fetchMore: handleFetchMore,
+                    hasLazyFetched: (hasLazyFetched.current = !!data),
+                    label: "More services",
+                    loading,
+                  }}
                 />
               )}
-            </Row>
-            <Row className="my-4">
-              <Col>
-                {productConnection?.pageInfo.hasNextPage ||
-                data?.service.products?.pageInfo ? (
-                  <MoreButton
-                    {...{
-                      customFetch: fetchMoreProducts,
-                      fetchMore: () =>
-                        fetchMore({
-                          variables: {
-                            serviceId: rest._id!,
-                            productArgs: {
-                              last: 20,
-                              before:
-                                data?.service.products?.pageInfo.endCursor,
-                            },
-                          },
-                        }),
-                      hasLazyFetched,
-                      label: "More services",
-                      loading,
-                    }}
-                  />
-                ) : null}
-              </Col>
-            </Row>
-          </Container>
-        )}
-      </Layout>
-    );
-  };
-
-export default ServicePage;
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </Layout>
+  );
+}
